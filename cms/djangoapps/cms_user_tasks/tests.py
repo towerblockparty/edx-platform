b01a504a957d0680582e5ef9b64037a6fcebf656
Awais Qureshi <awais.qureshi@arbisoft.com>
2020-09-15 15:06:21 +0500

    BOM-1921 Fixing the failure.

--- cms/djangoapps/cms_user_tasks/tests.py
+++ cms/djangoapps/cms_user_tasks/tests.py
@@ -62,11 +62,7 @@ def _data(response):
     return response.data
 
 
-@override_settings(BROKER_URL='memory://localhost/',
-                   CELERY_TASK_EAGER_PROPAGATES=True,
-                   CELERY_TASK_ALWAYS_EAGER=True,
-                    BROKER_BACKEND='memory'
-                   )
+@override_settings(BROKER_URL='memory://localhost/',CELERY_IGNORE_RESULT=True)
 class TestUserTasks(APITestCase):
     """
     Tests of the django-user-tasks REST API endpoints.
